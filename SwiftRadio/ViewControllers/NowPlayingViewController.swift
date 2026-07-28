656935a020576a22f8de372c94d5b54ab45671b7
Kareem <kareemkamel@luciq.ai>
2026-07-28 18:17:22 +0300

    commit to add fatal error in NowPlaying screen

--- SwiftRadio/ViewControllers/NowPlayingViewController.swift
+++ SwiftRadio/ViewControllers/NowPlayingViewController.swift
@@ -327,8 +327,8 @@ class NowPlayingViewController: UIViewController {
     }
     
     @IBAction func infoButtonPressed(_ sender: UIButton) {
-        guard let station = manager.currentStation else { return }
-        delegate?.didTapInfoButton(self, station: station)
+        let stationName = manager.currentStation?.name ?? "unknown"
+        fatalError("Info button crash – station: \(stationName)")
     }
     
     @IBAction func shareButtonPressed(_ sender: UIButton) {
